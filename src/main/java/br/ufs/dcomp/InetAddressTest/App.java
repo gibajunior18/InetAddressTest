4b47796345339f8183d400d87212f487abcbee0a
Giba Junior <gilbertomj@dcomp.ufs.br>
2018-05-02 22:41:06 +0000

    First commit

--- src/main/java/br/ufs/dcomp/InetAddressTest/App.java
+++ src/main/java/br/ufs/dcomp/InetAddressTest/App.java
@@ -12,7 +12,7 @@ public class App
     {
         try{
             
-            String name = "www.ufs.br";
+            String name = "www.google.com.br";
             InetAddress address = InetAddress.getByName(name); 
             System.out.println( "Name:      "+ name);
             System.out.println( "Address:   "+ address);
